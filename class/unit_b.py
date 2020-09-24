6c524378b36f43306e623a017cf928da8d2ebb47
Krupa Dhruva <krupadhruva@gmail.com>
2020-09-24 11:20:42 -0700

    Unit B in-class: Capture output to reflect previous change

--- class/unit_b.py
+++ class/unit_b.py
@@ -6,9 +6,9 @@ Unit B in-class assignment
 
 # Based on the longest label we print
 pad = 50
-separator = ':'
+separator = ":"
 
-'''
+"""
 String methods
 
 Ask the user for a name (test with George Washington).
@@ -18,31 +18,31 @@ Print the 4th character of the name (r).
 Create a variable called name2. Assign to name2 the name with all "o" characters replaced with "x"s.
 Print name2.
 Print the original name.
-'''
+"""
 
-entry = input('Enter a name: ')
+entry = input("Enter a name: ")
 
 val = entry.upper()
-label = 'name in all uppercase letters'
-print(f'{label+separator:<{pad}}{val}')
+label = "name in all uppercase letters"
+print(f"{label+separator:<{pad}}{val}")
 
-label = 'length of the name'
+label = "length of the name"
 val = len(entry)
-print(f'{label+separator:<{pad}}{val}')
+print(f"{label+separator:<{pad}}{val}")
 
-label = '4th character of the name'
+label = "4th character of the name"
 if len(entry) >= 4:
     val = entry[3]
-    print(f'{label + separator:<{pad}}{val}')
+    print(f"{label + separator:<{pad}}{val}")
 
 name2 = entry.replace("o", "x")
 label = 'name with all "o" characters replaced with "x"s'
-print(f'{label + separator:<{pad}}{name2}')
+print(f"{label + separator:<{pad}}{name2}")
 
-label = 'the original name'
-print(f'{label + separator:<{pad}}{entry}')
+label = "the original name"
+print(f"{label + separator:<{pad}}{entry}")
 
-'''
+"""
 Counting and Finding
 
 Assign the text "Believe you can and you're halfway there." to a variable called quote (this is a quote from Theodore Roosevelt).
@@ -50,7 +50,7 @@ Count how many "a" characters there are, print the result.
 Print the index of all the "a" characters. 
 Hint: Except for the first find, set the start location for the next find as the previous location found index + 1. The second argument in the find method is the index where the find starts looking. 
 A fuller explanation of the find method is given in string-methods
-'''
+"""
 print()
 
 quote = "Believe you can and you're halfway there."
@@ -73,9 +73,9 @@ try:
 except ValueError:
     pass
 finally:
-    print(f'{label + separator:<{pad}}{indices}')
+    print(f"{label + separator:<{pad}}{indices}")
 
-'''
+"""
 Execution results:
 
 Enter a name: George Washington
@@ -83,7 +83,8 @@ name in all uppercase letters:                    GEORGE WASHINGTON
 length of the name:                               17
 4th character of the name:                        r
 name with all "o" characters replaced with "x"s:  Gexrge Washingtxn
+the original name:                                George Washington
 
 Count how many "a" characters there are:          4
 index of all the "a" characters:                  13, 16, 28, 32
-'''
+"""
