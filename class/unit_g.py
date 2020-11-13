046230ac49b12c0fb13b827ee1907b8e73e2ae25
Krupa Dhruva <krupadhruva@gmail.com>
2020-11-12 16:39:53 -0800

    Unit G: File IO (csv) with dictionaries and sets

---
+++ class/unit_g.py
@@ -0,0 +1,159 @@
+"""
+Krupa Dhruva
+CIS 41A   Fall 2020
+Unit G in-class assignment
+"""
+
+from csv import DictReader
+
+"""
+Part One – Working with Files
+
+Create a new file called ZenOfPython.txt and write the first two lines of the Zen of Python (see The Zen of Python) to the file. Close the file.
+
+Reopen the file and append the 7th and the 17th lines. Then close the file.
+
+Open the file again and read and print the entire contents of the file (there shouldn't be any blank lines between the text). 
+Then close the file.
+
+Sample Execution Results:
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Readability counts.
+If the implementation is hard to explain, it's a bad idea.
+"""
+
+
+def zen_of_python():
+    """Performs file IO by writing lines, appending and reading"""
+    try:
+        # Open file for write, truncate if present
+        fh = open("ZenOfPython.txt", mode="w")
+        lines = ("Beautiful is better than ugly.", "Explicit is better than implicit.")
+        for line in lines:
+            fh.write(line + "\n")
+        fh.close()
+
+        # Open file for append
+        fh = open("ZenOfPython.txt", mode="a")
+        lines = (
+            "Readability counts.",
+            "If the implementation is hard to explain, it's a bad idea.",
+        )
+        for line in lines:
+            fh.write(line + "\n")
+        fh.close()
+
+        # Open file for read
+        fh = open("ZenOfPython.txt", mode="r")
+        for line in fh.readlines():
+            print(line, end="")
+        fh.close()
+    except OSError as ex:
+        print(f"error opening file for IO: {ex}")
+
+
+"""
+Part Two – CSV Files
+
+For this exercise, you will need to download the file Cities.csv from Canvas and save it into the same directory as your Python script. To do this, login to Canvas, select CIS 41A, select Files, select Cities.csv, select Download, and save into the same directory with your unit G in-class Python script.
+
+The file contains a list of cities, their state, and their population. The file starts with a header row of field names, which are City, State, Population.
+
+You will need to save this data (but not the header data) into a dictionary. The dictionary key will be a tuple consisting of the name of the city and the name of the state. The dictionary value will be the population. 
+The reason for this structure is that there are a number of duplicate city names within the file, but no duplicate city/state pairs.
+
+Hint: When you create your reader object, you should use the DictReader from the csv module. Because the file contains a header row of field names, we don’t have to explicitly define them when creating the reader object. However, you should use these field names instead of numerical indexes when working with the row data.
+
+After reading the csv file, iterate through the dictionary and print the data.
+
+Then, ask the user for a city and state, then print that city’s population, if it exists. 
+Test with Dublin, California.
+
+Sample Execution Results:
+
+Athens Georgia 115452
+Athens Ohio 23832
+Berlin Connecticut 19866
+Berlin Wisconsin 5524
+Dublin California 46036
+Dublin Ohio 41751
+and so on...
+
+Please enter a city: Dublin
+Please enter a state: California
+Dublin California has a population of 46036
+"""
+
+
+def population_distribution():
+    """Loads a CSV (comma separated file) with population distribution
+    across cities & states, prints and allows user query population for a city & state"""
+    try:
+        # Using context manager to automatically close file
+        with open("Cities.csv", mode="r") as fh:
+            dr = DictReader(fh)
+            population_map = {}
+            for row in dr:
+                key = (row[dr.fieldnames[0]], row[dr.fieldnames[1]])
+                val = row[dr.fieldnames[2]]
+                population_map[key] = val
+
+        for (city, state), population in population_map.items():
+            print(f"{city} {state} {population}")
+
+        print()
+        city = input("Please enter a city: ")
+        state = input("Please enter a state: ")
+        key = (city, state)
+        if key in population_map:
+            print(f"{city} {state} has a population of {population_map[key]}")
+        else:
+            print(f"Population for {city}, {state} not found")
+
+    except OSError as ex:
+        print(f"error opening file for read: {ex}")
+
+
+def main():
+    zen_of_python()
+
+    print()
+    population_distribution()
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+Execution results:
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Readability counts.
+If the implementation is hard to explain, it's a bad idea.
+
+Athens Georgia 115452
+Athens Ohio 23832
+Berlin Connecticut 19866
+Berlin Wisconsin 5524
+Dublin California 46036
+Dublin Ohio 41751
+Glasgow Connecticut 11951
+Glasgow Kentucky 14028
+London Kentucky 7993
+London Ohio 9904
+Milan Illinois 5099
+Milan Michigan 5836
+Milan Tennessee 7851
+Paris Kentucky 8553
+Paris Tennessee 10156
+Paris Texas 25171
+Warsaw Indiana 13559
+Warsaw New York 5064
+
+Please enter a city: Dublin
+Please enter a state: California
+Dublin California has a population of 46036
+"""
---
+++ home/unit_g.py
@@ -0,0 +1,176 @@
+"""
+Krupa Dhruva
+CIS 41A   Fall 2020
+Unit G take-home assignment
+"""
+
+"""
+Part One – Reading a data file
+
+For this exercise, you will need to download the file States.txt from Canvas and save it into the same directory as your Python script. To do this, login to Canvas, select CIS 41A, select Files, select States.txt, select Download, and save into the same directory with your unit G take-home Python script.
+
+The file has 50 lines of data, one for each state in the Unites States. Each line of data contains three pieces of data separated by a space: the two letter abbreviation of the state's name, the region that the state is in, and the 2016 population of the state.
+
+You need to find and print the state with the highest population in the Midwest region.
+
+Example output:
+
+Highest population state in the Midwest is: IL 12802000
+"""
+
+
+def build_population_distribution(filter_by_region=()):
+    try:
+        with open("States.txt", mode="r") as fh:
+            state_population_map = {}
+            for line in fh.readlines():
+                entries = line.rstrip("\r\n").split(" ")
+
+                # Apply filter if provided
+                if len(filter_by_region) == 0 or entries[1] in filter_by_region:
+                    state_population_map[entries[0]] = int(entries[2])
+
+            return state_population_map
+    except IndexError:
+        print("error in data, insufficient fields")
+    except OSError as ex:
+        print(f"error opening file for read: {ex}")
+
+
+"""
+Part Two – A Dictionary of Lists
+
+Download the file USPresidents.txt from from Canvas and save it into the same directory as your Python script. To do this, login to Canvas, select CIS 41A, select Files, select USPresidents.txt, select Download, and save into the same directory with your unit G take-home Python script.
+
+The file has 44 lines of data, one for each president in the history of the Unites States. Each line of data contains two pieces of data separated by a space: the two letter abbreviation of the name of the state where the president was born, and the name of the president (for your convenience, the president's name has been converted to a single string – George Washington has been converted to George_Washington).
+
+Using the data from the file, you need to build a dictionary of states and the presidents born in those states. Each key will be a state abbreviation and each value will be a list of presidents.
+
+After building the dictionary, determine the state with the most presidents and how many presidents were born there. Print their names.
+
+Example output:
+
+The state with the most presidents is VA with 8 presidents:
+George_Washington
+James_Madison
+James_Monroe
+John_Tyler
+And so on...
+"""
+
+
+def build_us_presidents_map():
+    """
+    Reads a file with president's birth state and president name
+
+    returns: Dictionary of state abbrev and presidents born in the state
+    """
+    try:
+        with open("USPresidents.txt", mode="r") as fh:
+            # Map of state and presidents
+            state_presidents = {}
+
+            for line in fh.readlines():
+                # Default split is on any white space (space or tab)
+                entries = line.rstrip("\r\n").split()
+
+                # Insert new entry or append to existing entry
+                if entries[0] in state_presidents:
+                    state_presidents[entries[0]].append(entries[1])
+                else:
+                    state_presidents[entries[0]] = [entries[1]]
+
+            return state_presidents
+    except IndexError as ex:
+        print(f"error in data, insufficient fields: {line}, {ex}")
+    except OSError as ex:
+        print(f"error opening file for read: {ex}")
+
+
+"""
+Part Three – Dictionary Keys and Sets
+
+Build a second dictionary from the USPresidents.txt file described in the previous exercise. Each key will again be a state abbreviation, however, the value will be the count of presidents from that state.
+
+Create a set of the ten most populous US states (CA, TX, FL, NY, IL, PA, OH, GA, NC, MI).
+
+Then create a new set that represents a set of populous US states that have had presidents born in them (you should be able to do this with one line of code).
+
+Print a count of this new set along with an alpha-sorted listing of these states and how many presidents have been born in them.
+
+Example output:
+
+8 of the 10 high population states have had presidents born in them:
+CA 1
+GA 1
+IL 1
+NC 2 
+And so on...
+"""
+
+
+def main():
+    # Part 1
+
+    # Filter states that are part of Midwest
+    midwest_population_map = build_population_distribution(filter_by_region=["Midwest"])
+
+    # Get the state with highest population
+    result = max(midwest_population_map.items(), key=lambda item: item[1])
+    print(f"Highest population state in the Midwest is: {result[0]} {result[1]}")
+
+    # Part 2
+    print()
+    state_presidents_map = build_us_presidents_map()
+    state_with_max_presidents = max(
+        state_presidents_map.items(), key=lambda item: len(item[1])
+    )
+    print(
+        f"The state with the most presidents is {state_with_max_presidents[0]} with {len(state_with_max_presidents[1])} presidents:"
+    )
+    print("\n".join(state_presidents_map[state_with_max_presidents[0]]))
+
+    # Part 3
+    print()
+    state_presidents_count = {k: len(v) for (k, v) in state_presidents_map.items()}
+    population_map = build_population_distribution()
+    top_10_populous_states = set(
+        sorted(population_map, key=population_map.get, reverse=True)[:10]
+    )
+
+    populous_states_with_presidents = top_10_populous_states & set(
+        state_presidents_map.keys()
+    )
+
+    for state in sorted(populous_states_with_presidents):
+        print(f"{state} {state_presidents_count.get(state)}")
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+Execution results:
+
+Highest population state in the Midwest is: IL 12802000
+
+The state with the most presidents is VA with 8 presidents:
+George_Washington
+James_Madison
+James_Monroe
+John_Tyler
+Thomas_Jefferson
+William_Henry_Harrison
+Woodrow_Wilson
+Zachary_Taylor
+
+CA 1
+GA 1
+IL 1
+NC 2
+NY 5
+OH 7
+PA 1
+TX 2
+"""
